090a3ab3d239f36932f13b61c4136cbea050ac03
zcp <z164757979@163.com>
2017-06-28 00:17:55 +0800

    完成上下左右键让小球移动

--- Roll a Ball/Assets/Scripts/PlayerController.cs
+++ Roll a Ball/Assets/Scripts/PlayerController.cs
@@ -12,19 +12,17 @@ public class PlayerController : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		rb = GetComponent<Rigidbody> ();
-		Console.WriteLine ("XXX");
 	}
 
-	// 在多帧中应用
-	void Fixedupdate () {
-		float moveHorizontal = Input.GetAxis ("Horizontal");
-		float moveVertical = Input.GetAxis ("Vertical");
+    private void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
 
-		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
-		Vector3 move = new Vector3 (10.0f, 0.0f, 10.0f);
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
 
-		// 添加力到刚体上
-		rb.AddForce (move * speed);
-		Console.WriteLine ("XXX");
-	}
+        // 添加力到刚体上
+        rb.AddForce(movement * speed);
+        // this.GetComponent<Rigidbody>().AddForce(move * speed);
+    }
 }
