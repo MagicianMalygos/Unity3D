3cb9e9d3f9c15257b2fb73fb38e93e7a46374f75
朱超鹏 <ZHUCHAOPENG388@pingan.com.cn>
2017-07-03 20:45:53 +0800

    finish Roll a Ball (I do it)

--- Roll a ball (I do it)/Assets/Scripts/PlayerController.cs
+++ Roll a ball (I do it)/Assets/Scripts/PlayerController.cs
@@ -1,16 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour {
 
 	public float speed;
+	public Text countText;
+	public Text winText;
 
 	private Rigidbody rb;
+	private int count;
 
 	// Use this for initialization
 	void Start () {
 		rb = GetComponent<Rigidbody> ();
+		count = 0;
+		SetCountText ();
+		winText.text = "";
 	}
 	
 	void FixedUpdate () {
@@ -19,5 +26,21 @@ public class PlayerController : MonoBehaviour {
 
 		Vector3 movement = new Vector3 (moveHorizontal, 0, moveVertical);
 		this.rb.AddForce (movement * speed);
+	}
+
+	void OnTriggerEnter (Collider other) {
+		if (other.gameObject.CompareTag ("Pick Up")) {
+			other.gameObject.SetActive (false);
+			count++;
+			SetCountText ();
+			Debug.Log ("Count: "+this.count);
+		}
+	}
+
+	void SetCountText () {
+		this.countText.text = "Count: "+this.count;
+		if (this.count >= 8) {
+			this.winText.text = "You Win!";
+		}
 	}
 }
