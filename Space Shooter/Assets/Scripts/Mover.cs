dfc63cc69aeb7b5cb417c678ab72bcf9a3866d97
朱超鹏 <ZHUCHAOPENG388@pingan.com.cn>
2017-07-05 20:32:36 +0800

    1.完成飞机移动 2.完成子弹的创建和移动 3.完成飞机发射子弹

---
+++ Space Shooter/Assets/Scripts/Mover.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour {
+
+	public float speed;
+
+	private Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+		this.rb = GetComponent<Rigidbody> ();
+		this.rb.velocity = transform.forward * speed;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Space Shooter/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Boundary {
+	public float xMin, xMax, zMin, zMax;
+}
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed;
+	public Boundary boundary;
+	public float tilt;
+
+	public GameObject shot;
+	public Transform shotSpawn;
+	public float fireRate;
+
+	private float nextFire;
+	private Rigidbody rb;
+
+	void Start () {
+		this.rb = GetComponent<Rigidbody> ();
+	}
+
+	void Update () {
+		if (Input.GetButton ("Fire1") && Time.time > nextFire) {
+			nextFire = Time.time + fireRate;
+			Instantiate (shot, shotSpawn.position, shotSpawn.rotation);
+		}
+	}
+
+	void FixedUpdate () {
+		// 控制移动
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+		rb.velocity = movement * speed;
+
+		// 控制不能超出边界
+		this.rb.position = new Vector3 (
+			Mathf.Clamp (this.rb.position.x, boundary.xMin, boundary.xMax), 
+			0.0f, 
+			Mathf.Clamp (this.rb.position.z, boundary.zMin, boundary.zMax)
+		);
+
+		// 移动时倾斜
+		this.rb.rotation = Quaternion.Euler (0.0f, 0.0f, this.rb.velocity.x * - tilt);
+	}
+}
